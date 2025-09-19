9098154b3f4cd92797eae05f80554b9021fff3b2
KOOl1BoY <yashodha1998@gmail.com>
2025-09-19 12:34:54 +0530

    yes

---
+++ int.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("size of int:%d\n",sizeof(int));
+    printf("size of short int:%d\n",sizeof(short int ));
+    printf("size of long int:%d\n",sizeof(long int));
+    printf("size of long long int:%d\n",sizeof(long long int));
+    printf("size of unsigned int:%d\n",sizeof(unsigned int));
+    printf("size that is mentioned is in bytes\n");
+    return 0;
+
+}
