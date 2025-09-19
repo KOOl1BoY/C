c053f28fd6c796c05911901c6285be59ccbc23c6
KOOl1BoY <yashodha1998@gmail.com>
2025-09-19 12:23:18 +0530

    done

---
+++ hello_world.c
@@ -0,0 +1,5 @@
+#include<stdio.h> //'#' means preprocessor directive, stdio.h is a standard library for input and output functions
+int main() {  // int main() is the main function where the execution of the program begins
+    printf("Hello, World!\n"); // printf is a function to print text to the console, \n is a newline character
+    return 0; // return 0 indicates that the program ended successfully. if it runs !=0 then program ended with an error
+}
