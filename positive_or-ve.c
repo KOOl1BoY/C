0d1406f173eccb1784634cc4a2e23bbe8012a179
KOOl1BoY <yashodha1998@gmail.com>
2025-09-21 20:31:28 +0530

    done

---
+++ positive_or-ve.c
@@ -0,0 +1,18 @@
+//program to check wheter given number is positive or negative
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number:");
+    scanf("%d",&a);
+    if(a>0)
+        printf("the number is positive");
+    else if (a==0)
+    {
+        printf("the number is zero");
+    }
+    
+    else
+        printf("the number is negative");
+    return 0;
+}
